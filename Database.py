d8f72c5e869c864dbb30fa65f0e371e7854afe3c
Vedanshi Srivastava <vedanshisrivastava1@gmail.com>
2026-09-06 14:06:11 +0530

    Connect app to Neon database

--- Database.py
+++ Database.py
@@ -1,19 +1,23 @@
 import psycopg2
+import streamlit as st
+
 
 def connection():
     con = psycopg2.connect(
-        host = "localhost",
-        database = "ecommerce",
-        user = "postgres",
-        password = "Gungun@31",
-        port = "5432",
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        port=st.secrets["DB_PORT"],
+        sslmode="require"
     )
 
-    if con : 
+    if con:
         print(">>>>> Connection Established!")
     else:
         print(">>>>> Connection Failed!")
+
     return con
 
+
 conn = connection()
-    
